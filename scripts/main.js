adf32a434fd2819204dca9b3b8d7c139bfe587b3
Joao-Barbalho <joaobarbalho.contato@gmail.com>
2023-01-07 21:19:35 -0400

    assignment portal first task

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+
+let currentDate = document.lastModified;
+document.querySelector('#lastUpdated').textContent = currentDate;
+
+// Copyright Date
+let date = new Date();
+let year = date.getFullYear();
+document.querySelector('#copyrightYear').innerHTML = year;
