c11aeb7b39f58b8c51644cb1bbe8d97173447813
Joao-Barbalho <joaobarbalho.contato@gmail.com>
2023-02-04 16:48:50 -0400

    chamber commerce update

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -21,3 +21,43 @@ hamburger.addEventListener('click', () => {
     nav.classList.toggle('responsive');
 
 });
+
+let day = date.getDay();
+
+if (day == 1 || day == 2) {
+    let banner = document.querySelector('.banner');
+    banner.style.display = 'block';
+
+    document.querySelector(".banner-button").addEventListener("click", function () {
+        banner.style.display = 'none';
+      });
+} else {
+    let banner = document.querySelector('.banner');
+    banner.style.display = 'none';
+}
+
+const images = document.querySelectorAll('img');
+const options = { threshold: 0.5, rootMargin: '0px 0px -100px 0px' };
+
+const preloadImage = (img) => {
+  const src = img.getAttribute('data-src');
+  if (!src) {
+    return;
+  }
+  img.src = src;
+}
+
+const io = new IntersectionObserver((entries, io) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            io.unobserve(entry.target);
+        }
+    });
+}, options);
+
+images.forEach(image => {
+    io.observe(image);
+});
